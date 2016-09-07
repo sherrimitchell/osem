349a449f3703ca5f1a9e59cf786f06a63701f5e8
Sherri Mitchell <rsamitchell@att.net>
2016-09-07 08:44:09 -0400

    added open graph meta data to event#show q

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,4 @@
 class Room < ActiveRecord::Base
-  belongs_to :venue
-
   has_many :event_schedules, dependent: :nullify
 
   has_paper_trail ignore: [:guid], meta: { conference_id: :conference_id }
